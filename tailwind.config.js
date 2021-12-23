a200ed6996f225e4bd8c2caac5c8fc2ce8102299
Hugo Suarez <hugo.suarez92@yahoo.com>
2021-12-23 16:21:51 -0500

    Using/adding plugins to tailwind css and implenting them

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+
+const plugin = require('tailwindcss/plugin')
+
+
+module.exports = {  
+  mode : 'jit',      
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [
+    plugin(function({addUtilities}){
+      const newUtilities = {
+        ".scale-1" : {
+          transform : 'scale(1)'
+        },
+        '.rotate-1': {
+          transform: 'rotate(1)'
+        }
+      }
+      addUtilities(newUtilities, {
+        variants:['responsive', 'hover']
+      })
+    })
+  ],
+}

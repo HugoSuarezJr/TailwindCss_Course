0b462cb290077bdf8b1400e528822bafd3280b03
Hugo Suarez <hugo.suarez92@yahoo.com>
2021-12-25 01:16:13 -0500

    Dark Mode:

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {  
-  mode : 'jit',      
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
     },
@@ -14,15 +13,6 @@ module.exports = {
   extend: {
     },
   },
-  plugins: [
-    plugin(function({addBase, theme}){
-      const heading = {
-        'h1' : {fontSize : theme('fontSize.2xl')},
-        'h3' : {fontSize : theme('fontSize.xl')},
-        'h6' : {fontSize : theme('fontSize.lg')}
-      }
-
-      addBase(heading)
-    })
-  ],
+  plugins: [],
 }
+ 
